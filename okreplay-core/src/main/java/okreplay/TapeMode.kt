fccb487435307938c84f47bbd4bd2c34e24677fd
Alexandre Ducatti <alexandredpl@gmail.com>
2021-08-22 12:41:02 -0400

    Added new tape mode READ_ONLY_IGNORE to proceed the request when no override is found

--- okreplay-core/src/main/java/okreplay/TapeMode.kt
+++ okreplay-core/src/main/java/okreplay/TapeMode.kt
@@ -1,18 +1,23 @@
 package okreplay
 
 enum class TapeMode(val isReadable: Boolean, val isWritable: Boolean, val isSequential: Boolean) {
-  UNDEFINED(false, false, false), READ_WRITE(true, true, false), READ_ONLY(true, false, false),
-  READ_ONLY_QUIET(true, false, false), READ_SEQUENTIAL(true, false, true), WRITE_ONLY(false,
-      true, false), WRITE_SEQUENTIAL(false, true, true);
+    UNDEFINED(false, false, false),
+    READ_WRITE(true, true, false),
+    READ_ONLY(true, false, false),
+    READ_ONLY_QUIET(true, false, false),
+    READ_SEQUENTIAL(true, false, true),
+    WRITE_ONLY(false, true, false),
+    WRITE_SEQUENTIAL(false, true, true),
+    READ_ONLY_IGNORE(true, false, false);
 
-  /**
-   * For compatibility with Groovy truth.
-   */
-  fun asBoolean(): Boolean {
-    return isReadable || isWritable
-  }
+    /**
+     * For compatibility with Groovy truth.
+     */
+    fun asBoolean(): Boolean {
+        return isReadable || isWritable
+    }
 
-  fun toNullable(): TapeMode? {
-    return if (this == UNDEFINED) null else this
-  }
+    fun toNullable(): TapeMode? {
+        return if (this == UNDEFINED) null else this
+    }
 }
